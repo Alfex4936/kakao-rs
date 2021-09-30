4c88e741bc3f630326a1fec759f0af897d702768
seok <ikr@kakao.com>
2021-09-30 12:12:19 +0900

    Test integrated button
    모든 버튼 하나의 struct로 테스트

--- src/components/buttons.rs
+++ src/components/buttons.rs
@@ -424,3 +424,118 @@ impl ShareButton {
 }
 
 /***** Buttons *****/
+
+#[derive(Serialize, Debug, Default)]
+#[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
+pub struct OneButton {
+    label: String,
+    action: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    phone_number: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    web_link_url: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    message_text: Option<String>,
+}
+
+pub enum ButtonType {
+    Call,
+    Share,
+    Link,
+    Text,
+}
+
+impl OneButton {
+    #[inline]
+    fn new(r#type: ButtonType) -> Self {
+        let action = match r#type {
+            ButtonType::Call => "phone",
+            ButtonType::Share => "share",
+            ButtonType::Link => "webLink",
+            ButtonType::Text => "message",
+        };
+        OneButton {
+            action: action.to_string(),
+            ..Default::default()
+        }
+    }
+
+    pub fn set_number<S: Into<String>>(mut self, number: S) -> Self {
+        self.phone_number = Some(number.into());
+        self
+    }
+
+    pub fn set_label<S: Into<String>>(mut self, label: S) -> Self {
+        self.label = label.into();
+        self
+    }
+
+    pub fn set_msg<S: Into<String>>(mut self, message: S) -> Self {
+        self.message_text = Some(message.into());
+        self
+    }
+
+    pub fn set_link<S: Into<String>>(mut self, link: S) -> Self {
+        self.web_link_url = Some(link.into());
+        self
+    }
+}
+
+impl<'de> Deserialize<'de> for OneButton {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let text: Map<String, Value> = Map::deserialize(deserializer)?;
+        let mut keys = HashMap::new();
+        for (key, value) in &text {
+            let _value = value.as_str().unwrap();
+            keys.insert(key.to_owned(), _value.to_string());
+        }
+
+        let mut button: OneButton = match text.get("action").unwrap().as_str() {
+            Some("webLink") => OneButton::new(ButtonType::Link),
+            Some("share") => OneButton::new(ButtonType::Share),
+            Some("message") => OneButton::new(ButtonType::Text),
+            Some("phone") => OneButton::new(ButtonType::Call),
+            _ => panic!("Unknown button type"),
+        };
+
+        if let Some(l) = keys.get("label") {
+            button.label = l.to_string();
+        }
+        if let Some(l) = keys.get("webLinkUrl") {
+            button.web_link_url = Some(l.to_string());
+        }
+        if let Some(l) = keys.get("messageText") {
+            button.message_text = Some(l.to_string());
+        }
+        if let Some(l) = keys.get("phoneNumber") {
+            button.phone_number = Some(l.to_string());
+        }
+
+        Ok(button)
+    }
+}
+
+#[test]
+fn test_button() {
+    // let a = OneButton::new(ButtonType::Call)
+    //     .set_label("전화하기")
+    //     .set_number("911");
+    // let b = OneButton::new(ButtonType::Share).set_label("공유하기");
+    // let c = OneButton::new(ButtonType::Link)
+    //     .set_label("링크열기")
+    //     .set_link("https://");
+    // let d = OneButton::new(ButtonType::Text)
+    //     .set_label("그냥 버튼")
+    //     .set_msg("발화문임");
+
+    let data = r#"[{"label":"CALL LABEL","action":"phone","phoneNumber":"0","messageText":"MESSAGE"},{"label":"SHARE LABEL","action":"share"},{"label":"MSG LABEL","action":"message"},{"label":"LABEL","action":"webLink","webLinkUrl":"https://"}]"#;
+    let buttons: Vec<OneButton> = serde_json::from_str(data).unwrap();
+
+    assert_eq!(buttons.len(), 4);
+    assert_eq!(buttons[0].action, "phone");
+    assert_eq!(serde_json::to_string(&buttons).expect("Failed"), data); // Serialize
+}
